8088a27f2c75d6cfe1831cd77b36205c119362e0
TAHA <dg0882318@gmail.com>
2026-05-25 23:48:28 +0500

    Done

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
 console.log("Hello World!");
-
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
